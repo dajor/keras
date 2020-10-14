081ab6ea394d603cd6ac6255d9d4b553f95f353b
Amier Haider <amier.haider@gmail.com>
2020-10-14 14:29:21 +0200

    Bug fix

--- predict.py
+++ predict.py
@@ -8,6 +8,9 @@ import argparse
 from invoicenet import FIELDS
 from invoicenet.acp.acp import AttendCopyParse
 import ocrmypdf
+import tensorflow as tf
+from tika import parser
+
 
 
 def main():
@@ -59,14 +62,6 @@ def make_predictions(invoice, field, pred_dir):
     paths = []
     paths.append(invoice)
     #ocr_pdf(invoice)
-    model_ocr = model.load('models/ocr_model')
-    model_image = model.load('models/images_model')
-
-    model_ocr.predict()
-
- 
-
-    print(pd.DataFrame(sklearn.metrics.confusion_matrix(y_test, rnn_pred), columns=CLASS_NAMES, index=CLASS_NAMES))
 
 
     model = AttendCopyParse(field=field, restore=True)
@@ -90,11 +85,54 @@ def make_predictions(invoice, field, pred_dir):
         print("{}: {}\n".format(field, prediction))
 
     print("Predictions stored in '{}'".format(pred_dir))
-    
+
+def extract_all_data(invoice, pred_dir):
+    fields = [
+    'invoice_number',
+    'vendor_name',
+    'invoice_date',
+    'tax_amount',
+    'total_amount']
+ 
+ 
+    opdf = ocr_pdf(invoice)
+ 
+    for field in fields:
+        make_predictions(invoice, field, pred_dir)
+
+
+def make_predictions(invoice, field, pred_dir):
+    paths = []
+    paths.append(invoice)
+
+
+    model = AttendCopyParse(field=field, restore=True)
+    print("\nExtracting field '{}' from {} invoices...\n".format(field, len(paths)))
+
+    predictions = model.predict(paths=paths)
+    for prediction, filename in zip(predictions, paths):
+        filename = os.path.basename(filename)[:-3] + 'json'
+        labels = {}
+        if os.path.exists(os.path.join(pred_dir, filename)):
+            with open(os.path.join(pred_dir, filename), 'r') as fp:
+                labels = json.load(fp)
+        with open(os.path.join(pred_dir, filename), 'w') as fp:
+            labels[field] = prediction
+            fp.write(json.dumps(labels))
+        print("\nFilename: {}".format(filename))
+        print("{}: {}\n".format(field, prediction))
+
+    print("Predictions stored in '{}'".format(pred_dir))
 
 def ocr_pdf(pdf_document):
-    new_pdf_doc = ocrmypdf.ocr(pdf_document)
-    new_pdf_doc.save(pdf_document)
+    try:
+        dir_name = os.path.dirname(pdf_document)
+        filename = os.path.basename(pdf_document)[:-4] + '_ocr.pdf'
+        new_pdf_doc = os.path.join(dir_name, filename)
+        res = ocrmypdf.ocr(pdf_document,new_pdf_doc,use_threads=True )
+        return new_pdf_doc
+    except Exception as e:
+        print(e)
 
 
 
